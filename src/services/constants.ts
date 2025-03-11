d99e4eacd498a7d1e2e67dd6e3c1f75a852da2c5
Daniil Fomenko <danil.fomchik@gmail.com>
2025-03-11 22:53:55 +0200

    adding items to localStorage

---
+++ src/services/constants.ts
@@ -0,0 +1,14 @@
+export const defaultColumns = [
+    {
+        id: '2ac6a58c7f3f4a5a',
+        title: 'To Do',
+    },
+    {
+        id: 'ac6a58c7f3f4a5ac',
+        title: 'In Progress',
+    },
+    {
+        id: '02ac6a58c7f3f4a5',
+        title: 'Done',
+    },
+];
